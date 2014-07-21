453f2836922aeaae01c420f12459a351b4e8992e
Shawn Woodford <shawn@kingandpartners.com>
2014-07-21 17:05:23 -0400

    many improvements, fix bug with volume names that contain numbers

--- ec2-create-snapshots.sh
+++ ec2-create-snapshots.sh
@@ -3,31 +3,42 @@
 # TODO: Convert from EC2 CLI to AWS CLI
 # TODO: Add error handling and loop breaks
 
+DESCRIBEVOLUMES=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1")
 
-TOTALBACKUPVOLUMES=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | grep Name | cut -f 3 | nl | wc -l)
-# echo $TOTALBACKUPVOLUMES
+TOTALBACKUPVOLUMES=$(echo "$DESCRIBEVOLUMES" | grep Name | cut -f 3 | nl | wc -l)
+
+echo " "
+echo "====================================================="
+echo "Creating EC2 Snapshots for Volumes with tag Backup=1"
+echo "Snapshots to be created:"$TOTALBACKUPVOLUMES
+echo "====================================================="
+echo " "
+
 START=1
-
 for (( COUNT=$START; COUNT<=$TOTALBACKUPVOLUMES; COUNT++ ))
 do
   echo "====================================================="
   echo \#$COUNT
-  # DESCRIBEVOLUMES=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1")
-  # VOLUME=$(echo $DESCRIBEVOLUMES | cut -f 8 | nl | grep -w $COUNT | cut -f 2)
-  VOLUME=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | cut -f 9 | nl | grep -w $COUNT | cut -f 2)
+  
+  VOLUME=$(echo "$DESCRIBEVOLUMES" | cut -f 9 | nl | grep -w $COUNT | cut -f 2)
+  # VOLUME=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | cut -f 9 | nl | grep -w $COUNT | cut -f 2)
   echo "Volume ID: "$VOLUME
-  # NAME=$(echo $DESCRIBEVOLUMES | grep Name | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
-  NAME=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | grep Name | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
+  
+  NAME=$(echo "$DESCRIBEVOLUMES" | grep Name | cut -f 3 | nl | grep -w [^0-9]$COUNT | cut -f 2)
+  # NAME=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | grep Name | cut -f 3 | nl | grep -w [^0-9]$COUNT | cut -f 2)
   echo "Volume Name: "$NAME
-  # CLIENT=$(echo $DESCRIBEVOLUMES | grep Client | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
-  CLIENT=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | grep Client | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
+  
+  CLIENT=$(echo "$DESCRIBEVOLUMES" | grep Client | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
+  # CLIENT=$(aws ec2 describe-volumes --filter Name=tag:Backup,Values="1" | grep Client | cut -f 3 | nl | grep -w $COUNT | cut -f 2)
   echo "Client: "$CLIENT
+  
   DESCRIPTION=$NAME-$(date +%m-%d-%Y)
-  echo "Snapshot Name: "$DESCRIPTION
+  echo "Snapshot Description: "$DESCRIPTION
 
   # TODO: Convert from EC2 CLI to AWS CLI
   SNAPSHOTRESULT=$(ec2-create-snapshot $VOLUME -d $DESCRIPTION)
-  # echo $SNAPSHOTRESULT
+  # echo "Snapshot result is: "$SNAPSHOTRESULT
+
   SNAPSHOTID=$(echo $SNAPSHOTRESULT | cut -c 10-23)
   echo "Snapshot ID: "$SNAPSHOTID
   #echo $SNAPSHOTID | grep -E "snap-........"
@@ -35,10 +46,9 @@ do
 
   # TODO: Convert from EC2 CLI to AWS CLI
   TAGRESULT=$(ec2-create-tags $SNAPSHOTID --tag Client=$CLIENT --tag Name=$NAME)
-
-  # echo "Snapshot result is: "$SNAPSHOTRESULT
-  # echo $TAGRESULT
-  # echo "Snapshot ID is: "$SNAPSHOTID
   # echo "Tag Result is: "$TAGRESULT
 done
-echo "Completed! Total Snapshots Created:"$TOTALBACKUPVOLUMES
+
+echo "====================================================="
+echo " "
+echo "Completed!"
