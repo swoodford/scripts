2928404549adc99e0db503a73da36921effdba58
Shawn Woodford <shawn@kingandpartners.com>
2014-12-19 09:52:46 -0500

    improve credential check

--- aws/ec2-delete-snapshots.sh
+++ aws/ec2-delete-snapshots.sh
@@ -3,11 +3,11 @@
 
 # Verify AWS CLI Credentials are setup
 # http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
-if ! [ -f ~/.aws/config ]; then
-  if ! [ -f ~/.aws/credentials ]; then
-    echo "Error: AWS config not found or CLI not installed."
-    exit 1
-  fi
+if ! grep -q aws_access_key_id ~/.aws/config; then
+	if ! grep -q aws_access_key_id ~/.aws/credentials; then
+		echo "Error: AWS config not found or CLI not installed. Please run \"aws configure\"."
+		exit 1
+	fi
 fi
 
 SNAPSHOTDATES=$(aws ec2 describe-snapshots --filters Name=tag:SnapshotCreation,Values="Automatic" --output text | grep SnapshotDate | cut -f 3 | sort -u)
